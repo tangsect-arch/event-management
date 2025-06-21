1d888c3d9934debb3e8a2c0ff48ea931d01a1fe3
Sabari <sabari288@gmail.com>
2025-06-21 16:15:08 +0530

    middleware implementation

--- app.mjs
+++ app.mjs
@@ -21,7 +21,7 @@ app.use(cors(corsConfifg));
 app.use(helmet());
 
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/admin", adminRouter);
-app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", verifyAdmin, adminRouter);
+app.use("/api/v1/user", authMiddleware, userRouter);
 
 export default app;
